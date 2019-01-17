89951ec156cd9eef8cc98616c80cacb358d3c5c0
Aman Kubanychbek <akubanychbek@klika-tech.com>
2019-01-17 20:01:44 +0300

    add nested state machine sample

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,18 +1,83 @@
 import {Machine} from 'xstate'
 import {interpret} from 'xstate/lib/interpreter'
 
-const toggleMachine = Machine({
-  initial: 'inactive',
-  states: {
-    inactive: {on: {TOGGLE: 'active'}},
-    active: {on: {TOGGLE: 'inactive'}},
-  },
-})
+// MESSAGES - USER DECISION
+const say = 'say'
+const yes = 'yes'
+const no = 'no'
+const start = 'start'
+const done = 'done'
+const skip = 'skip'
 
-const toggleService = interpret(toggleMachine)
-  .onTransition(state => console.log(state.value))
+// EVENTS - SYSTEM DECISION
+const hasUnresolved = 'hasUnresolved'
+const allResolved = 'allResolved'
+
+// STATES
+const askUser = 'askUser'
+const listen = 'listen'
+const searchVars = 'searchVars'
+const resovleVars = 'resolveVars'
+const finish = 'finish'
+const idle = 'idle'
+const listAllSteps = 'listAllSteps'
+const defineStep = 'defineStep'
+const ON = 'ON'
+const WHEN = 'WHEN'
+const DO = 'DO'
+const description = 'description'
+
+const section = {
+  initial: askUser,
+  states: {
+    askUser: {on: {yes: listen}},
+    listen: {on: {done: searchVars, say: listen}},
+    searchVars: {on: {hasUnresolved: resovleVars, allResolved: finish}},
+    resolveVars: {on: {done: finish, skip: finish}},
+    finish: {on: {say: finish}},
+  },
+}
+
+const step = {
+  initial: askUser,
+  states: {
+    askUser: {on: {yes: description}},
+    description: {on: {done: ON, say: description}},
+    ON: {on: {done: WHEN}, ...section},
+    WHEN: {on: {done: DO}, ...section},
+    DO: {on: {done: finish}, ...section},
+    finish: {on: {say: finish}},
+  },
+}
+
+const list = {
+  initial: askUser,
+  states: {
+    askUser: {on: {yes: listAllSteps, no: idle}},
+    listAllSteps: {on: {done: defineStep}},
+    defineStep: {on: {done: finish}, ...step},
+    idle: {on: {start: listAllSteps, say: idle}},
+    finish: {on: {say: finish}},
+  },
+}
+
+const machine = Machine(list)
+
+const log: string[] = []
+
+const rootService = interpret(machine)
+  .onTransition(state => log.push(state.event.type))
   .start()
 
-toggleService.send('TOGGLE').value // ?
+function send(msg: string) {
+  rootService.send(msg)
 
-toggleService.send('TOGGLE').value // ?
+  return rootService.state
+}
+
+rootService.state //?$.value
+send('no') //?$.value
+send('start') //?$.value
+send('blah') //?$.value
+send('done') //?$.value
+log
